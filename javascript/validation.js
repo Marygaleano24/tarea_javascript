1df59df6caa2051a019ef108e7514a85aeb42a5a
MaryGaleano24 <marianagaleano846@gmail.com>
2024-06-27 10:51:49 -0400

    validation.js modificado

--- javascript/validation.js
+++ javascript/validation.js
@@ -1,8 +1,12 @@
 function emailValidation() {
     const form = document.getElementById('form');
+    const emailField = document.getElementById('email');
+    const confirmEmailField = document.getElementById('email_confirm');
+
     form.addEventListener('submit', e => {
         e.preventDefault();
-        if (form.email.value !== form.email_confirm.value) {
+        if (emailField.value !== confirmEmailField.value) {
+            confirmEmailField.classList.add('error');
             const element = document.createElement('p')
             const message = document.createTextNode("Eメールが一致しません")
             form.appendChild(element);
@@ -10,9 +14,22 @@ function emailValidation() {
             element.classList.add("alert");
             setTimeout(() => {
                 form.removeChild(element)
-            }, 3000)
+            }, 3000);
         } else {
+            confirmEmailField.classList.remove('error');
             form.submit();
+        }
+    });
+
+    emailField.addEventListener('input', () => {
+        if (emailField.value === confirmEmailField.value) {
+            confirmEmailField.classList.remove('error');
+        }
+    });
+
+    confirmEmailField.addEventListener('input', () => {
+        if (emailField.value === confirmEmailField.value) {
+            confirmEmailField.classList.remove('error');
         }
     });
 };
